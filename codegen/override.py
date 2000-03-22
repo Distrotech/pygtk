fced78b6e1ad0dbf9450d5a3d3350e495d619c69
James Henstridge <james@daa.com.au>
2000-03-22 17:26:30 +0000

    simple module for parsing a file containing names of ignored functions and
    2000-03-23  James Henstridge  <james@daa.com.au>
    
    	* codegen/override.py: simple module for parsing a file containing
    	names of ignored functions and overriden implementations.
    
    	* gtk/__init__.py: add an __init__ module so the gtk directory acts
    	like a package.

---
+++ codegen/override.py
@@ -0,0 +1,37 @@
+# -*- Mode: Python; py-indent-offset: 4 -*-
+
+# this file contains code for loading up an override file.  The override file
+# provides implementations of functions where the code generator could not
+# do its job correctly.
+
+import sys, string
+
+class Overrides:
+	def __init__(self, fp=sys.stdin):
+		self.ignores = {}
+		self.overrides = {}
+		# read all the components of the file ...
+		bufs = map(string.strip, string.split(fp.read(), '%%'))
+		if buf == ['']: return
+		for buf in bufs:
+			self.__parse_override(buf)
+	def __parse_override(self, buffer):
+		pos = string.find(buffer, '\n')
+		if pos:
+			line = buffer[:pos]
+			rest = buffer[pos+1:]
+		else:
+			line = buffer ; rest = ''
+		words = string.split(line)
+		if words[0] == 'ignore':
+			for func in words[1:]: self.ignores[func] = 1
+		elif words[0] == 'override':
+			func = words[1]
+			self.overrides[func] = rest
+
+	def is_ignored(self, name):
+		return self.ignores.has_key(name)
+	def is_overriden(self, name):
+		return self.overrides.has_key(name)
+	def override(self, name):
+		return self.overrides[name]
---
+++ gtk/__init__.py
@@ -0,0 +1,10 @@
+# -*- Mode: Python; py-indent-offset: 4 -*-
+
+# load the required modules:
+from GTK import *
+import GDK
+from _gtk import *
+
+# for importing ...
+__all__ = ['_gtk', 'GTK', 'GDK' ]
+
